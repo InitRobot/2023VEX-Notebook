2b2b20b21d67d85ccff5c36632a8e945d509abc5
zhuyechen-xnwy <zhuyechen071025@126.com>
2023-10-18 21:04:24 +0800

    test

---
+++ include/robot-config.h
@@ -0,0 +1,20 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor Motor18;
+extern motor Motor10;
+extern motor Motor1;
+extern motor Motor2;
+extern motor Motor3;
+extern motor Motor4;
+extern motor Motor5;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\zhuye                                            */
+/*    Created:      Wed Oct 18 2023                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Motor18              motor         18              
+// Motor10              motor         10              
+// Motor1               motor         1               
+// Motor2               motor         2               
+// Motor3               motor         3               
+// Motor4               motor         4               
+// Motor5               motor         5               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+  while(1){
+    ///////////////底盘//////////////////////////////////////////////    
+    float axis3 = Controller1.Axis3.position(percent);
+    float axis4 = Controller1.Axis4.position(percent);
+
+    float trans_fb = axis3;
+    float rot = axis4;
+
+    Motor1.spin(forward, 128*trans_fb + 128*rot ,voltageUnits::mV);
+    Motor2.spin(forward, -128*trans_fb + 128*rot ,voltageUnits::mV);  
+    
+    wait(5,msec);
+    ///////////////////////////////旋臂/////////////////////////////////
+    float axis2 = Controller1.Axis2.position(percent);
+    
+    float rot_xuan_bi = axis2;
+
+    Motor3.spin(forward, -100*rot_xuan_bi ,voltageUnits::mV);
+    Motor4.spin(forward, 100*rot_xuan_bi ,voltageUnits::mV);  
+    
+    wait(5,msec);
+    //////////////////////收集/////////////////////////////////////////
+    if (Controller1.ButtonR2.pressing()) {
+      Motor5.spin(forward, 10.0, volt);
+    }
+    if (Controller1.ButtonL2.pressing()) {
+      Motor5.stop();
+    }
+    wait(5, msec);
+  }
+}
