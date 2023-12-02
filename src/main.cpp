d521ef91c010ed1bf399157caef3c5eae162c705
hs150521 <hs150521@163.com>
2023-12-02 20:10:34 +0800

    提高可维护性，添加了滚筒代码（未完善）

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,13 @@
 
 using namespace vex;
 
+const int VOLTAGE3 = 128;//3位数电压上限，方便*100
+const int VOLTAGE5 = 12800;//5位数电压上限
+float sensitivity = 1;//底盘移动灵敏度(0,1]
+int roller_dir = 0;//滚筒方向 -1外 0停 1内
+
 //循环内变量需要提前定义，在循环内重新定义会增加运算量
 float axis1,axis2,axis3,axis4,chassis_L_volt,chassis_R_volt;
-
-//定义电压上限
-const int VOLTAGE3 = 128;//3位数，方便*100
-const int VOLTAGE5 = 12800;//5位数
-
-float sensitivity=1;//底盘移动灵敏度(0,1]
 
 competition Competition;//定义比赛
 controller Controller1 = controller(primary);//定义遥控器
@@ -24,26 +23,73 @@ motor chassis_RF = motor(PORT4, ratio6_1, true);
 motor chassis_RBU = motor(PORT5, ratio6_1, false);
 motor chassis_RBD = motor(PORT6, ratio6_1, true);
 motor_group chassis_R = motor_group(chassis_RF, chassis_RBU, chassis_RBD);
+motor roller = motor(PORT7, ratio18_1, false);
 
 //底盘移动函数，添加了防止过载的处理
-void chassis_move(float left_volt, float right_volt)
+void chassis_move()
 {
-  if (left_volt > VOLTAGE5)
+  chassis_L_volt = VOLTAGE3 * axis1 + VOLTAGE3 * axis2 * sensitivity;
+  chassis_R_volt = VOLTAGE3 * axis1 - VOLTAGE3 * axis2 * sensitivity;
+  if (chassis_L_volt > VOLTAGE5)
   {
     chassis_L.spin(forward, VOLTAGE5, voltageUnits::mV);
-    chassis_R.spin(forward, right_volt / left_volt * VOLTAGE5, voltageUnits::mV);
+    chassis_R.spin(forward, chassis_R_volt / chassis_L_volt * VOLTAGE5, voltageUnits::mV);
   }
-  else if(right_volt > VOLTAGE5)
+  else if(chassis_R_volt > VOLTAGE5)
   {
-    chassis_L.spin(forward, left_volt / right_volt * VOLTAGE5, voltageUnits::mV);
+    chassis_L.spin(forward, chassis_L_volt / chassis_R_volt * VOLTAGE5, voltageUnits::mV);
     chassis_R.spin(forward, VOLTAGE5, voltageUnits::mV);
   }
   else
   {
-    chassis_L.spin(forward, left_volt, voltageUnits::mV);
-    chassis_R.spin(forward, right_volt, voltageUnits::mV);
+    chassis_L.spin(forward, chassis_L_volt, voltageUnits::mV);
+    chassis_R.spin(forward, chassis_R_volt, voltageUnits::mV);
   }
-  return ;
+  return;
+}
+
+// 摇杆位置更新
+void axis_update()
+{
+  axis1 = Controller1.Axis1.position(percent);
+  axis2 = Controller1.Axis2.position(percent);
+  axis3 = Controller1.Axis3.position(percent);
+  axis4 = Controller1.Axis4.position(percent);
+  /*
+  //调试用：输出摇杆位置
+  Controller1.Screen.clearScreen();
+  Controller1.Screen.setCursor(4,1);
+  Controller1.Screen.print("%f %f", axis1, axis2);
+  wait(50, msec);
+  */
+  return;
+}
+
+//滚筒向外移动
+void roller_up()
+{
+  if(roller_dir!=-1) roller_dir--;
+  Controller1.Screen.print("%d", roller_dir);
+  return;
+}
+
+//滚筒向内移动
+void roller_down()
+{
+  if(roller_dir!=1) roller_dir++;
+  Controller1.Screen.print("%d", roller_dir);
+  return;
+}
+
+//滚筒移动
+void roller_move()
+{
+  //若按钮按下则更新roller_dir，详见README
+  Controller1.ButtonUp.pressed(roller_up);
+  Controller1.ButtonDown.pressed(roller_down);
+  //更新旋转放下
+  roller.spin(forward, VOLTAGE5*roller_dir, voltageUnits::mV);
+  return;
 }
 
 //Pre-Autonomous Functions
@@ -63,26 +109,9 @@ void usercontrol(void) {
   // User control code here, inside the loop
   while (1) 
   {
-    //更新摇杆位置
-    axis1 = Controller1.Axis1.position(percent);
-    axis2 = Controller1.Axis2.position(percent);
-    axis3 = Controller1.Axis3.position(percent);
-    axis4 = Controller1.Axis4.position(percent);
-
-    /*
-    //调试用：输出摇杆位置
-    Controller1.Screen.clearScreen();
-    Controller1.Screen.setCursor(4,1);
-    Controller1.Screen.print("%f %f", axis1, axis2);
-    wait(50, msec);
-    */
-
-    //底盘移动
-    chassis_L_volt = VOLTAGE3 * axis1 + VOLTAGE3 * axis2 * sensitivity;
-    chassis_R_volt = VOLTAGE3 * axis1 - VOLTAGE3 * axis2 * sensitivity;
-    chassis_move(chassis_L_volt, chassis_R_volt);
-    //chassis_L.spin(forward, axis1*VOLTAGE3, voltageUnits::mV);
-    //chassis_R.spin(forward, axis2*VOLTAGE3, voltageUnits::mV);
+    axis_update();//更新摇杆位置
+    chassis_move();//底盘移动
+    roller_move();//滚筒移动
 
     wait(20, msec); // Sleep the task for a short amount of time to prevent wasted resources.
   }
