9b0631a2d67664349b4d4773b738a50b1c02e87d
hs150521 <hs150521@163.com>
2023-12-02 19:24:03 +0800

    添加移动

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,105 @@
-#pragma region VEXcode Generated Robot Configuration
-#pragma region VEXcode Generated Robot Configuration
-// Make sure all required headers are included.
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
-#include <string.h>
-
-
 #include "vex.h"
 
 using namespace vex;
 
-// Brain should be defined by default
-brain Brain;
+//循环内变量需要提前定义，在循环内重新定义会增加运算量
+float axis1,axis2,axis3,axis4,chassis_L_volt,chassis_R_volt;
 
-//定义intake模式
-int intake_mode = 0;//0：无模式，1：暂存，2：发射
+//定义电压上限
+const int VOLTAGE3 = 128;//3位数，方便*100
+const int VOLTAGE5 = 12800;//5位数
 
-// START V5 MACROS
-#define waitUntil(condition)                                                   \
-  do {                                                                         \
-    wait(5, msec);                                                             \
-  } while (!(condition))
+float sensitivity=1;//底盘移动灵敏度(0,1]
 
-#define repeat(iterations)                                                     \
-  for (int iterator = 0; iterator < iterations; iterator++)
-// END V5 MACROS
+competition Competition;//定义比赛
+controller Controller1 = controller(primary);//定义遥控器
 
+//定义电机，详见README
+//L:left R:right F:front B:back U:up D:down
+motor chassis_LF = motor(PORT1, ratio6_1, true);
+motor chassis_LBU = motor(PORT2, ratio6_1, false);
+motor chassis_LBD = motor(PORT3, ratio6_1, true);
+motor_group chassis_L = motor_group(chassis_LF, chassis_LBU, chassis_LBD);
+motor chassis_RF = motor(PORT4, ratio6_1, true);
+motor chassis_RBU = motor(PORT5, ratio6_1, false);
+motor chassis_RBD = motor(PORT6, ratio6_1, true);
+motor_group chassis_R = motor_group(chassis_RF, chassis_RBU, chassis_RBD);
 
-// Robot configuration code.
-/*vex-vision-config:begin*/
-vision::signature Vision13__BLUE_BALL = vision::signature (1, -2705, -1873, -2289,7615, 10485, 9050,2.1, 0);
-vision Vision13 = vision (PORT13, 50, Vision13__BLUE_BALL);
-/*vex-vision-config:end*/
-#pragma endregion VEXcode Generated Robot Configuration
-
-// Include the V5 Library
-#include "vex.h"
-  
-// Allows for easier use of the VEX Library
-using namespace vex;
-
-int Brain_precision = 0, Console_precision = 0, Vision13_objectIndex = 0;
-
-float myVariable;
-
-// "when started" hat block
-int whenStarted1() {
-  //printf("1");
-  Brain.Screen.print("VEXcode");
-  
-  
-  int j = 0;
-  while(true){
-    Vision13.takeSnapshot(Vision13__BLUE_BALL);
-    j++;
-    Brain.Screen.setCursor(1, 1);
-    //Brain.Screen.print('1');
-    //Brain.Screen.newLine();
-    Brain.Screen.print("N: %d", Vision13.objectCount);
-    Brain.Screen.newLine();
-    for(int i = 0; i < Vision13.objectCount; i++) {
-      Brain.Screen.print("X: %d", Vision13.objects[i].centerX);
-      Brain.Screen.newLine();
-      Brain.Screen.print("Y: %d", Vision13.objects[i].centerY);
-      Brain.Screen.newLine();
-    }
-    Brain.Screen.print(j);
-    wait(0.1, seconds);
-    Brain.Screen.clearScreen();
-
+//底盘移动函数，添加了防止过载的处理
+void chassis_move(float left_volt, float right_volt)
+{
+  if (left_volt > VOLTAGE5)
+  {
+    chassis_L.spin(forward, VOLTAGE5, voltageUnits::mV);
+    chassis_R.spin(forward, right_volt / left_volt * VOLTAGE5, voltageUnits::mV);
   }
-  
-  return 0;
+  else if(right_volt > VOLTAGE5)
+  {
+    chassis_L.spin(forward, left_volt / right_volt * VOLTAGE5, voltageUnits::mV);
+    chassis_R.spin(forward, VOLTAGE5, voltageUnits::mV);
+  }
+  else
+  {
+    chassis_L.spin(forward, left_volt, voltageUnits::mV);
+    chassis_R.spin(forward, right_volt, voltageUnits::mV);
+  }
+  return ;
 }
 
+//Pre-Autonomous Functions
+void pre_auton(void) 
+{
+  
+}
 
+//Autonomous Functions
+void autonomous(void) 
+{
+
+}
+
+//User Contorl Functions
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) 
+  {
+    //更新摇杆位置
+    axis1 = Controller1.Axis1.position(percent);
+    axis2 = Controller1.Axis2.position(percent);
+    axis3 = Controller1.Axis3.position(percent);
+    axis4 = Controller1.Axis4.position(percent);
+
+    /*
+    //调试用：输出摇杆位置
+    Controller1.Screen.clearScreen();
+    Controller1.Screen.setCursor(4,1);
+    Controller1.Screen.print("%f %f", axis1, axis2);
+    wait(50, msec);
+    */
+
+    //底盘移动
+    chassis_L_volt = VOLTAGE3 * axis1 + VOLTAGE3 * axis2 * sensitivity;
+    chassis_R_volt = VOLTAGE3 * axis1 - VOLTAGE3 * axis2 * sensitivity;
+    chassis_move(chassis_L_volt, chassis_R_volt);
+    //chassis_L.spin(forward, axis1*VOLTAGE3, voltageUnits::mV);
+    //chassis_R.spin(forward, axis2*VOLTAGE3, voltageUnits::mV);
+
+    wait(20, msec); // Sleep the task for a short amount of time to prevent wasted resources.
+  }
+}
+
+// Main will set up the competition functions and callbacks.
 int main() {
-  whenStarted1();
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) 
+  {
+    wait(100, msec);
+  }
 }
