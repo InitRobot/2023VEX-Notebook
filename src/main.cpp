ef0103290f4b91ef77709d043b4cd66798a00597
hs150521 <hs150521@163.com>
2023-12-02 15:38:45 +0800

    new car

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,82 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       C:\Users\zhuye                                            */
-/*    Created:      Wed Oct 18 2023                                           */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
+#pragma region VEXcode Generated Robot Configuration
+#pragma region VEXcode Generated Robot Configuration
+// Make sure all required headers are included.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
 
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// Motor18              motor         18              
-// Motor10              motor         10              
-// Motor1               motor         1               
-// Motor2               motor         2               
-// Motor3               motor         3               
-// Motor4               motor         4               
-// Motor5               motor         5               
-// ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 
 using namespace vex;
 
-int main() {
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
+// Brain should be defined by default
+brain Brain;
+
+//定义intake模式
+int intake_mode = 0;//0：无模式，1：暂存，2：发射
+
+// START V5 MACROS
+#define waitUntil(condition)                                                   \
+  do {                                                                         \
+    wait(5, msec);                                                             \
+  } while (!(condition))
+
+#define repeat(iterations)                                                     \
+  for (int iterator = 0; iterator < iterations; iterator++)
+// END V5 MACROS
+
+
+// Robot configuration code.
+/*vex-vision-config:begin*/
+vision::signature Vision13__BLUE_BALL = vision::signature (1, -2705, -1873, -2289,7615, 10485, 9050,2.1, 0);
+vision Vision13 = vision (PORT13, 50, Vision13__BLUE_BALL);
+/*vex-vision-config:end*/
+#pragma endregion VEXcode Generated Robot Configuration
+
+// Include the V5 Library
+#include "vex.h"
   
-  while(1){
-    ///////////////底盘//////////////////////////////////////////////    
-    float axis3 = Controller1.Axis3.position(percent);
-    float axis4 = Controller1.Axis4.position(percent);
+// Allows for easier use of the VEX Library
+using namespace vex;
 
-    float trans_fb = axis3;
-    float rot = axis4;
+int Brain_precision = 0, Console_precision = 0, Vision13_objectIndex = 0;
 
-    Motor1.spin(forward, 128*trans_fb + 95*rot ,voltageUnits::mV);
-    Motor2.spin(forward, -128*trans_fb + 95*rot ,voltageUnits::mV);  
-    
-    wait(5,msec);
-    ///////////////////////////////旋臂/////////////////////////////////
-    float axis2 = Controller1.Axis2.position(percent);
-    
-    float rot_xuan_bi = axis2;
+float myVariable;
 
-    Motor3.spin(forward, -95*rot_xuan_bi ,voltageUnits::mV);
-    Motor4.spin(forward, 95*rot_xuan_bi ,voltageUnits::mV);  
-    
-    wait(5,msec);
-    //////////////////////收集/////////////////////////////////////////
-    if (Controller1.ButtonR2.pressing()) {
-      Motor5.spin(forward, 10.0, volt);
+// "when started" hat block
+int whenStarted1() {
+  //printf("1");
+  Brain.Screen.print("VEXcode");
+  
+  
+  int j = 0;
+  while(true){
+    Vision13.takeSnapshot(Vision13__BLUE_BALL);
+    j++;
+    Brain.Screen.setCursor(1, 1);
+    //Brain.Screen.print('1');
+    //Brain.Screen.newLine();
+    Brain.Screen.print("N: %d", Vision13.objectCount);
+    Brain.Screen.newLine();
+    for(int i = 0; i < Vision13.objectCount; i++) {
+      Brain.Screen.print("X: %d", Vision13.objects[i].centerX);
+      Brain.Screen.newLine();
+      Brain.Screen.print("Y: %d", Vision13.objects[i].centerY);
+      Brain.Screen.newLine();
     }
-    if (Controller1.ButtonL2.pressing()) {
-      Motor5.stop();
-    }
-    wait(5, msec);
+    Brain.Screen.print(j);
+    wait(0.1, seconds);
+    Brain.Screen.clearScreen();
+
   }
+  
+  return 0;
+}
+
+
+int main() {
+  whenStarted1();
 }
